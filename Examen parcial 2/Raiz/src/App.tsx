895614a239f5af97f184b2d5d52807831e178a9c
IvanArevalos <ivanarevalos@isft220.edu.ar>
2025-10-30 12:07:32 -0300

    Subido el 2do parcial de desarrollo aplicativos

---
+++ Examen parcial 2/Raiz/src/App.tsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import { Characterlist } from './Characterslist';
+export default function MyApp(){
+  return(
+    <div className="App">
+    <h1>Api de rick y morty</h1>
+    <Characterlist/>
+    </div>
+);
+}
